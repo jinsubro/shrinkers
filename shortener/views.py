547c2ad20cbdab7836bb3c7894ec49b95aff9e67
jinsubro <jinsubro@gmail.com>
2022-08-30 12:33:16 +0900

    CH02_04 Login, Logout, Forms - Completed

--- shortener/views.py
+++ shortener/views.py
@@ -1,14 +1,15 @@
+from django.contrib.auth.forms import AuthenticationForm
 from django.http.response import JsonResponse
 from shortener.models import Users
 from django.shortcuts import redirect, render
 from django.views.decorators.csrf import csrf_exempt
-
+from shortener.forms import RegisterForm
+from django.contrib.auth import login, authenticate, logout
 
 # Create your views here.
 
 
 def index(request):
-    print(request.user.pay_plan.name)
     user = Users.objects.filter(id=request.user.id).first()
     email = user.email if user else "Anonymous User!"
     print("Logged in?", request.user.is_authenticated)
@@ -32,3 +33,43 @@ def get_user(request, user_id):
             user = Users.objects.filter(pk=user_id).update(username=username)
 
         return JsonResponse(dict(msg="You just reached with Post Method!"))
+
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        msg = "올바르지 않은 데이터 입니다."
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password1")
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            msg = "회원가입완료"
+        return render(request, "register.html", {"form": form, "msg": msg})
+    else:
+        form = RegisterForm()
+        return render(request, "register.html", {"form": form})
+
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        msg = "가입되어 있지 않거나 로그인 정보가 잘못 되었습니다."
+        print(form.is_valid)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            raw_password = form.cleaned_data.get("password")
+            user = authenticate(username=username, password=raw_password)
+            if user is not None:
+                msg = "로그인 성공"
+                login(request, user)
+        return render(request, "login.html", {"form": form, "msg": msg})
+    else:
+        form = AuthenticationForm()
+        return render(request, "login.html", {"form": form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("index")
--- shrinkers/urls.py
+++ shrinkers/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 import debug_toolbar
 from django.conf.urls import include
 from shortener.views import index
-from shortener.views import index, get_user
+from shortener.views import index, get_user, register, login_view, logout_view
 from django.contrib import admin
 from django.urls import path
 
@@ -24,6 +24,9 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("__debug__/", include(debug_toolbar.urls)),  # Django Debug Tool
     path("", index, name="index"),
+    path("register", register, name="register"),
+    path("login", login_view, name="login"),
+    path("logout", logout_view, name="logout"),
     path("get_user/<int:user_id>", get_user),
 ]
 
